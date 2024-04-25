3a5e427208018525fd6bd2bc1e2c58ac758f4efb
shparknr <shparknr11@naver.com>
2024-04-25 11:11:23 +0900

    docs : axios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import ScssComponent from "./components/ScssComponent";
 import CSSModule from "./components/CSSModule";
 import StyledComponent from "./components/StyledComponent";
+import Axios from "./pages/Axios";
 
 function App() {
   return (
     <div>
-      <StyledComponent />
+      <Axios />
     </div>
   );
 }
